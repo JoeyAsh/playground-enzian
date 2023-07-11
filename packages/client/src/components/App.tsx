9cd5e2cd1cc3724e1c6daa01769d6788322deda6
Johannes Aschenbrenner <johannes.aschenbrenner@wechselgott.com>
2023-07-11 23:48:52 +0200

    Basic setup and update eslint

---
+++ packages/client/src/components/App.tsx
@@ -0,0 +1,9 @@
+import {Box, Typography} from '@mui/material';
+
+export const App = () => {
+    return (
+        <Box>
+            <Typography>Playground notifier in Enzianweg</Typography>
+        </Box>
+    );
+};
